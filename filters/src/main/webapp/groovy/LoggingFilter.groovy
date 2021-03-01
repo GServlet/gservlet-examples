107c5f46f277ad7b4f9830a4f54c83df3e59bc0a
mamadou lamine ba <laminba2003@gmail.com>
2021-03-01 23:45:14 +0100

    adding the filters examples

--- filters/src/main/webapp/groovy/LoggingFilter.groovy
+++ filters/src/main/webapp/groovy/LoggingFilter.groovy
@@ -3,24 +3,21 @@ import org.gservlet.annotation.Filter
 @Filter("/*")
 class LoggingFilter {
 
-  void init() {
-    logger.info("loggingFilter initialized")
-  }
+	void init() {
+		logger.info("loggingFilter initialized")
+	}
 
-  void filter() {
-    request.parameterNames.each {
-      name ->
-        logger.info("Request Param : $name=$request[name]")
-    }
-    request.cookies.each {
-      cookie ->
-        logger.info("Cookie : $cookie.name=$cookie.value")
-    }
-    next()
-  }
+	void filter() {
+		request.parameterNames.each { name ->
+			logger.info("Request Param : $name=$request[name]")
+		}
+		request.cookies.each { cookie ->
+			logger.info("Cookie : $cookie.name=$cookie.value")
+		}
+		next()
+	}
 
-  void destroy() {
-
-  }
-
+	void destroy() {
+	}
+	
 }
