b97f55158f3827424d834aa0dd4beeb593dbcc2c
Mamadou Lamine Ba <laminba2003@gmail.com>
2021-03-01 23:42:27 +0100

    Update LoggingFilter.groovy

--- filters/src/main/webapp/groovy/LoggingFilter.groovy
+++ filters/src/main/webapp/groovy/LoggingFilter.groovy
@@ -2,23 +2,25 @@ import org.gservlet.annotation.Filter
 
 @Filter("/*")
 class LoggingFilter {
-	
-    void init() {
-		logger.info("loggingFilter initialized")
-	}
 
-	void filter() {
-		request.parameterNames.each { name ->
-			logger.info("Request Param : $name=$request[name]")
-		}
-		request.cookies.each { cookie ->
-			logger.info("Cookie : $cookie.name=$cookie.value")
-		}
-		next()
-	}
+  void init() {
+    logger.info("loggingFilter initialized")
+  }
 
-	void destroy() {
-		
-	}
+  void filter() {
+    request.parameterNames.each {
+      name ->
+        logger.info("Request Param : $name=$request[name]")
+    }
+    request.cookies.each {
+      cookie ->
+        logger.info("Cookie : $cookie.name=$cookie.value")
+    }
+    next()
+  }
+
+  void destroy() {
+
+  }
 
 }
